9302ce50ccfcc6824844d24ce2b92d3c0c487a94
Felipe Pinheiro <santospinheiro627@gmail.com>
2025-01-07 21:01:27 -0300

    up

--- pty-change.php
+++ pty-change.php
@@ -1,6 +1,53 @@
 <?php
 
-fuction get_all_post_types(): void 
-{
-    return [get_post_types()];
+/**
+ * Post Type Change
+ *
+ * Plugin Name: PostType Change
+ * Plugin URI: 
+ * Description:
+ * Version:     1.0.
+ * Author:      Felipe Pinheiro
+ * Author URI:  https://github.com/felipesantos2
+ * Text Domain: post-type-change
+ * Requires at least: 4.9
+ * Requires PHP: 5.2.4
+ */
+
+if (! defined('ABSPATH')) {
+    throw new \Exeception('Constants is not defined !');
 }
+
+if (! class_exists('Post_type_Change')) {
+    class Post_type_Change
+    {
+        public static function initialize(): void
+        {
+            add_action('admin_menu', static::register_the_page() );
+        }
+
+        public static function register_the_page()
+        {
+            add_menu_page(
+                'tools.php',
+                'Felipe',
+                'Felipe',
+                'manage_options',
+                'my-custom-submenu-page',
+                static::page_content()
+            );
+        }
+
+        public static function page_content()
+        {
+?>
+            <div class="wrap">
+                <h2>Page Title</h2>
+            </div>
+<?php
+        }
+    }
+}
+
+
+Post_type_Change::initialize();
